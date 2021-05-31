e5785b6634d9f3b34464b59000af05ca00c3d335
vinhng2301 <ducvinhnguyen0201@gmail.com>
2021-05-31 16:03:24 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    LikeComponent
   ],
   imports: [
     BrowserModule,
